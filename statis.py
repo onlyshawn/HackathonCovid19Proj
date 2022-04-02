a1497436fa9a3e596f66254fc68c7649fc1421e8
onlyshawn <65843537+onlyshawn@users.noreply.github.com>
2022-04-02 15:27:36 -0400

    Update statis.py

--- statis.py
+++ statis.py
@@ -1,10 +1,10 @@
 import xlrd
-#打开excel
+
 wb = xlrd.open_workbook('dataset_registry.xlsx')
-#按工作簿定位工作表
+
 sh = wb.sheet_by_name('MosMedData COVID19_1110')
-print(sh.nrows)#有效数据行数
-print(sh.ncols)#有效数据列数
+print(sh.nrows)
+print(sh.ncols)
 labels = {'CT-0':0,'CT-1':0,'CT-2':0,'CT-3':0,'CT-4':0}
 
 for i in range(sh.nrows):
