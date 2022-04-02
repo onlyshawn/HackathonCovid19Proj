070f390577f79c6801ecbcc4f2ce757e80a34637
onlyshawn <65843537+onlyshawn@users.noreply.github.com>
2022-04-02 15:25:29 -0400

    Update DataLoader.py

--- DataLoader.py
+++ DataLoader.py
@@ -20,17 +20,17 @@ def getImage(src):
     img = nib.load(img_path).get_fdata()
     # print(img.shape)
     img_3d_max = np.amax(img)
-    img = img / img_3d_max * 255  # 对所求的像素进行归一化变成0-255范围,这里就是三维数据
-    # for i in range(img.shape[2]):   # 对切片进行循环
-    #     img_2d = img[:, :, i]  # 取出一张图像
-    #    # plt.imshow(img_2d) 显示图像
+    img = img / img_3d_max * 255  # Normalization
+    # for i in range(img.shape[2]):   # 
+    #     img_2d = img[:, :, i]  # take one piece of image
+    #    # plt.imshow(img_2d) show image
     #    # plt.pause(0.001)
     #     # filter out 2d images containing < 10% non-zeros
     #     # print(np.count_nonzero(img_2d))
     #     #print("before process:", img_2d.shape)
-    #     if float(np.count_nonzero(img_2d)) / img_2d.size >= preserving_ratio:  # 表示一副图像非0个数超过整副图像的10%我们才把该图像保留下来
-    #         img_2d = img_2d / 127.5 - 1  # 对最初的0-255图像进行归一化到[-1, 1]范围之内
-    #         img_2d = np.transpose(img_2d, (1, 0))  # 这个相当于将图像进行旋转90度
+    #     if float(np.count_nonzero(img_2d)) / img_2d.size >= preserving_ratio:  # 
+    #         img_2d = img_2d / 127.5 - 1  # 
+    #         img_2d = np.transpose(img_2d, (1, 0))  # rotate 90 degrees
     #         # plt.imshow(img_2d)
     #         # plt.pause(0.01)
 
